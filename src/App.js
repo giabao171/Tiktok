0b2d2d8be0ffd71dd34191e3045c0e100f2bf04c
giabaolaptrinh <giabaon171@gmail.com>
2023-01-29 18:50:31 +0700

    'deploy'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes/routes';
 import { DefaultLayout } from './layouts';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyDUuSkdOM7c6UamWtO4_l-URCorNu_26tY',
+    authDomain: 'tiktok-8bc34.firebaseapp.com',
+    projectId: 'tiktok-8bc34',
+    storageBucket: 'tiktok-8bc34.appspot.com',
+    messagingSenderId: '989386334865',
+    appId: '1:989386334865:web:dab99adb5517a9fd9d5530',
+    measurementId: 'G-EX6LQRWVTJ',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
     return (
         <Router>
